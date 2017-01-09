26829225e9a89717c4cda348d5baa1078a61f965
Tanuja <paturi.tanuja123@gmail.com>
2017-01-09 17:07:26 +0530

    register

--- WebContent/app.js
+++ WebContent/app.js
@@ -2,16 +2,36 @@ var app = angular.module("myApp", [ "ngRoute", "ngCookies", "blogapp" ]).run(
 		run);
 app.config(function($routeProvider, $locationProvider) {
 
-	$routeProvider.when("/", {
-		templateUrl : "Login.html",
+	$routeProvider.when("/home", {
+		templateUrl : "Home.html",
 		controller : 'LoginController',
-		
-	}).when("/register", {
+		controllerAs : 'vm'
+	})
+	.when("/register", {
 		templateUrl : "Users/register.html",
-		controller : "RegisterController"
-	}).when("/login", {
-		templateUrl : "Login/Login.html",
-		controller : 'LoginController',
-		
+		controller : "userctrl"
 	});
+	console.log("route");
 });
+run.$inject = [ '$rootScope', '$location', '$cookieStore', '$http' ];
+function run($rootScope, $location, $cookieStore, $http) {
+	// keep user logged in after page refresh
+	$rootScope.globals = $cookieStore.get('globals') || {};
+	$rootScope.currentuser = $cookieStore.get('currentuser') || {};
+	if ($rootScope.globals.currentUser) {
+		$http.defaults.headers.common['Authorization'] = 'Basic '
+				+ $rootScope.globals.currentUser.authdata; // jshint
+		// ignore:line
+	}
+
+	$rootScope.$on('$locationChangeStart', function(event, next, current) {
+		// redirect to login page if not logged in and trying to access a
+		// restricted page
+		var restrictedPage = $.inArray($location.path(), [ 
+				'/register', '/home' ]) === -1;
+		var loggedIn = $rootScope.globals.currentUser;
+		if (restrictedPage && !loggedIn) {
+			$location.path('/');
+		}
+	});
+}
